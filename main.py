1ebd545f748f983dd2584aff8cc1ada0dc38e4c0
Renan Viana <renanjviana@gmail.com>
2023-11-12 10:44:46 -0300

    init: Inicialização do projeto

---
+++ main.py
@@ -0,0 +1,11 @@
+# Para onde vai as instalações dos pacotes quando não tem um ambiente virtual
+
+# Com o venv, o site-packages fica dentro do divetório do projeto
+
+import site
+import sys
+
+# print(site.getsitepackages())
+
+for path in sys.path:
+    print(path)
